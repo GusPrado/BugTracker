e0dde0e1a376bfb773ff7b0f953f470ac9a2e8a4
Gus Prado <contato@gusprado.com>
2019-11-10 19:49:05 -0300

    Added email feature and Bootstrap layout

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require ('express')
 const path = require ('path')
 const bodyParser = require('body-parser')
+const { promisify } = require('util')
+const sgMail = require('@sendgrid/mail');
 
 const GoogleSpreadsheet = require('google-spreadsheet')
 const credentials = require('./bugtrackerkey.json')
@@ -15,37 +17,63 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //Configurations
 const docId = '1cI8_UL1DCiXH8eNsQWScFutl3PJvm9FscHFgWNmr2P0'
 const worksheetIndex = 0
+const sendGridKey = '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'
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.post('/', (req, res) => {
-
+app.post('/', async (req, res) => {
+    try{
     const doc = new GoogleSpreadsheet(docId)
-    doc.useServiceAccountAuth(credentials, (err) => {
-    if (err) {
-        console.log('Spreadsheet could not be opened')
-    } else {
-        console.log('Spreadsheet opened')
-        doc.getInfo((err, info) => {
-            const { name, email, type, reproduce, expected, output } = req.body
-            const worksheet = info.worksheets[worksheetIndex]
-            worksheet.addRow({ 
+    await promisify(doc.useServiceAccountAuth)(credentials)
+    //console.log('Spreadsheet opened')
+    const info = await promisify(doc.getInfo)()    
+    const { name, email, type, reproduce, expected, output, userAgent, userDate } = req.body
+    let { source } = req.query
+    if (!source){   
+        source = 'direct'
+    }
+    console.log(source)
+    const worksheet = info.worksheets[worksheetIndex]
+    await promisify(worksheet.addRow)({ 
                 name,
                 email,
                 type,
                 reproduce,
                 expected,
                 output,
-            }, err => {
-                res.send('Bug reported!')
+                source,
+                userAgent,
+                userDate
             })
-        })
+
+        // If CRITICAL
+        // using Twilio SendGrid's v3 Node.js Library
+        // https://github.com/sendgrid/sendgrid-nodejs
+        // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        if (type === 'critical'){
+            sgMail.setApiKey(sendGridKey);
+            const msg = {
+            to: 'gustavo.gpprado@gmail.com',
+            from: `${email}`,
+            subject: 'CRITICAL Bug reported',
+            text: `
+                The user ${name} has reported a problem.
+            `,
+            html: `
+            The user ${name} has reported a problem.
+            `,
+            };
+            await sgMail.send(msg);
+        } 
+
+            res.render('success')
+    }catch(err){
+        res.send('Error on sending form.')
+        console.log(err)
     }
 })
-
-    
-})
+        
 
 app.listen(3000, () => console.log('App running on port #3000'))
--- test-spreadsheet.js
+++ test-spreadsheet.js
@@ -1,17 +1,29 @@
 const GoogleSpreadsheet = require('google-spreadsheet')
 const credentials = require('./bugtrackerkey.json')
+const { promisify } = require('util')
 
-const doc = new GoogleSpreadsheet('1cI8_UL1DCiXH8eNsQWScFutl3PJvm9FscHFgWNmr2P0')
-doc.useServiceAccountAuth(credentials, (err) => {
-    if (err) {
-        console.log('Spreadsheet could not be opened')
-    } else {
-        console.log('Spreadsheet opened')
-        doc.getInfo((err, info) => {
-            const worksheet = info.worksheets[0]
-            worksheet.addRow({ name: 'Gus2', email: 'gus2@test.com' }, err => {
-                console.log('Line added')
-            })
-        })
-    }
-})
+const addRowToSheet = async() => {
+    const doc = new GoogleSpreadsheet('1cI8_UL1DCiXH8eNsQWScFutl3PJvm9FscHFgWNmr2P0')
+    await promisify(doc.useServiceAccountAuth)(credentials)
+    console.log('Spreadsheet opened')
+    const info = await promisify(doc.getInfo)()
+    const worksheet = info.worksheets[0]
+    await promisify(worksheet.addRow)({ name: 'Gus2', email: 'gus2@test.com' })
+}
+
+addRowToSheet()
+
+// const doc = new GoogleSpreadsheet('1cI8_UL1DCiXH8eNsQWScFutl3PJvm9FscHFgWNmr2P0')
+// doc.useServiceAccountAuth(credentials, (err) => {
+//     if (err) {
+//         console.log('Spreadsheet could not be opened')
+//     } else {
+//         console.log('Spreadsheet opened')
+//         doc.getInfo((err, info) => {
+//             const worksheet = info.worksheets[0]
+//             worksheet.addRow({ name: 'Gus2', email: 'gus2@test.com' }, err => {
+//                 console.log('Line added')
+//             })
+//         })
+//     }
+// })
